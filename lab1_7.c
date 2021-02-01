1e842145c1dd343869e9e8f154c8bdc555a383f2
Paphatsinee <paphatsinee.r@ku.th>
2021-02-01 20:05:04 +0700

    Create lab1_7.c

---
+++ lab1_7.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    float time;
+    scanf("%f",&time);
+    if (time<12)
+    {
+        printf("%.2f a.m.",time);
+    }
+    else if(time>12)
+    {
+        printf("%.2f p.m.",time-12);
+    }
