7d9b82b6de23c142efe70d83a0ed6b133bacf2c1
Paphatsinee <paphatsinee.r@ku.th>
2021-02-01 18:03:40 +0700

    Create lab1_3.c

---
+++ lab1_3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,b,sum;
+    scanf("%d",&n);
+    if(n>=0&&n<=1000000000)
+    {
+        if(b>=0&&b<=1000000000)
+        {
+            sum=a+b;
+            printf("%d",sum);
+        }
+    }
+    return 0;
+}
