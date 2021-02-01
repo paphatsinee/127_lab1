6f4b13a02098827284c5d6476a228dbec6c46e59
Paphatsinee <paphatsinee.r@ku.th>
2021-02-01 19:27:24 +0700

    Create lab1_6.c

---
+++ lab1_6.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,m,t;
+    char a;
+    scanf("%d %d %d",&n,&m,&t);
+    fflush(stdin)
+    scanf("%c",&a)
+     for(i = 0; i < N; i++)
+    {
+        scanf("%d", &n);
+
+        if(i==0 || n > max)
+        {
+            max = n;
+            lo = i+1;
+        }
+    }
+     printf("%d %d",lo,max);
+
+    return 0;
+}
