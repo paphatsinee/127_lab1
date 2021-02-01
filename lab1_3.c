8dd98c2dfb8452b428870639b85df80441fe1b2d
Paphatsinee <paphatsinee.r@ku.th>
2021-02-01 18:28:03 +0700

    Update lab1_3.c

--- lab1_3.c
+++ lab1_3.c
@@ -5,11 +5,8 @@ int main()
 
     long int n,m,sum;
     scanf("%ld",&n);
-    if(n>=0&&n<pow(10,1000000))
-    {
-            sum=(n%3);
-            m=(n%11);
-            printf("%ld %ld",sum,m);
-    }
+
+    printf("%ld %ld",n%3,n%11);
+
     return 0;
 }
